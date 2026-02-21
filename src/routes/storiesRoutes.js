b9471ae532011539c590e4e0ea350611e862a281
Anna-Makovska <brain.lady404@gmail.com>
2026-02-21 20:19:50 +0200

    update and create stories

--- src/routes/storiesRoutes.js
+++ src/routes/storiesRoutes.js
@@ -1,12 +1,34 @@
-import { Router } from 'express';
+﻿import { Router } from 'express';
 import { celebrate } from 'celebrate';
-import { getStories, getOwnStories } from '../controllers/storiesController.js';
-import { getStoriesSchema, getOwnStoriesSchema } from '../validations/storiesValidation.js';
+import { getStories, getOwnStories, createStory, updateStory } from '../controllers/storiesController.js';
+import {
+  getStoriesSchema,
+  getOwnStoriesSchema,
+  createStorySchema,
+  updateStorySchema,
+} from '../validations/storiesValidation.js';
 import { authenticate } from '../middleware/authenticate.js';
+import { upload } from '../middleware/multer.js';
 
 const router = Router();
 
 router.get('/stories', celebrate(getStoriesSchema), getStories);
 router.get('/stories/own', authenticate, celebrate(getOwnStoriesSchema), getOwnStories);
+
+router.post(
+  '/stories',
+  authenticate,
+  upload.single('img'),
+  celebrate(createStorySchema),
+  createStory,
+);
+
+router.patch(
+  '/stories/:storyId',
+  authenticate,
+  upload.single('img'),
+  celebrate(updateStorySchema),
+  updateStory,
+);
 
 export default router;

6708ead0125e3e5cfbd56eeb1787e7dbf0bf040d
Roman <roman.anokhin.work@gmail.com>
2026-02-25 16:35:46 +0200

    Fix/add localhost to cors

--- src/server.js
+++ src/server.js
@@ -19,7 +19,12 @@ const PORT = process.env.PORT ?? 3000;
 
 // Middleware
 app.use(logger);
-app.use(cors());
+app.use(cors({
+    origin: [
+      "http://localhost:3000",
+    ],
+    credentials: true,
+  }));
 app.use(express.json());
 app.use(cookieParser());
 

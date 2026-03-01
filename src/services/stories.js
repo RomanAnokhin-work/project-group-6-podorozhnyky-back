33e7dfb776f6590a614efdffdd7cc644841c9a2c
StunMasteR <162857412+StunMasteR@users.noreply.github.com>
2026-03-01 18:44:48 +0200

    fixd

--- src/services/stories.js
+++ src/services/stories.js
@@ -2,11 +2,16 @@ import { Story } from '../models/story.js';
 
 
 
-export const getStoryByIdService = async (storyId) => {
-  const story = await Story.findById(storyId)
-    .populate({ path: 'ownerId', select: 'name' })
-    .populate({ path: 'category', select: 'name' })
-    .lean();
+// export const getStoryByIdService = async (storyId) => {
+//   const story = await Story.findById(storyId)
+//     .populate({ path: 'ownerId', select: 'name' })
+//     .populate({ path: 'category', select: 'name' })
+//     .lean();
 
-  return story;
+//   return story;
+// };
+export const getStoryByIdService = async (storyId) => {
+  return await Story.findById(storyId)
+    .populate('ownerId', 'name email')
+    .populate('category', 'name');
 };

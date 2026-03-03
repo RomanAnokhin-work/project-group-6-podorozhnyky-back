ab202cde1d52d2388472aebcf8f188464db01804
Roman <roman.anokhin.work@gmail.com>
2026-03-03 18:44:35 +0200

    Fix/ChangeGetOwnStoriesSchema

--- src/validations/storiesValidation.js
+++ src/validations/storiesValidation.js
@@ -12,7 +12,7 @@ export const getStoriesSchema = {
 export const getOwnStoriesSchema = {
   [Segments.QUERY]: Joi.object({
     page: Joi.number().integer().min(1).default(1),
-    perPage: Joi.number().integer().min(5).max(20).default(10),
+    perPage: Joi.number().integer().min(3).max(20).default(10),
   }),
 };
 

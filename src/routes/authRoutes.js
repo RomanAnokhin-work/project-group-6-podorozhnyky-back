48ca97c5a91de1427f7c7720fb5e3447d513487e
Roman <roman.anokhin.work@gmail.com>
2026-03-05 03:03:28 +0200

    Fix/addResetPasswordAndResetEmail

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,6 +1,8 @@
 import express from 'express';
-import { registerUser, loginUser, logoutUser, refreshUserSession } from '../controllers/authController.js';
+import { registerUser, loginUser, logoutUser, refreshUserSession, resetPassword, requestResetEmail  } from '../controllers/authController.js';
 import { authenticate } from '../middleware/authenticate.js';
+import { celebrate } from 'celebrate';
+import {  requestResetEmailSchema, resetPasswordSchema } from '../validations/authValidation.js';
 
 
 
@@ -16,5 +18,12 @@ authRouter.post('/auth/logout', authenticate, logoutUser);
 
 //ПРИВАТНИЙ ендпоінт для оновлення сесії користувача
 authRouter.post('/auth/session', refreshUserSession);
+
+//ПУБЛІЧНИЙ ендпоінт для зміни пошти
+authRouter.post('/auth/request-reset-email', celebrate(requestResetEmailSchema), requestResetEmail);
+
+//ПУБЛІЧНИЙ ендпоінт для зміни паролю
+authRouter.post('/auth/reset-password', celebrate(resetPasswordSchema), resetPassword);
+
 
 export default authRouter;
